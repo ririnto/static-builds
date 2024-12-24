dde60dd19cb06f54a3c1a219610fcde7973d8cdc
ririnto <ririnto@kakao.com>
2024-12-25 01:53:58 +0900

    빌드 캐시 사용을 늘리기 위한 변경

--- haproxy/pre-download.sh
+++ haproxy/pre-download.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 . .env
-curl -L -o "haproxy-${HAPROXY_VERSION}.tar.gz" "https://www.haproxy.org/download/3.0/src/haproxy-${HAPROXY_VERSION}.tar.gz"
+curl --fail -L -o "haproxy-${HAPROXY_VERSION}.tar.gz" "https://www.haproxy.org/download/3.0/src/haproxy-${HAPROXY_VERSION}.tar.gz"
--- nginx/pre-download.sh
+++ nginx/pre-download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 . .env
-curl -L -o "nginx-${NGINX_VERSION}.tar.gz" "https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
-curl -L -o "nginx-module-vts-${NGINX_MODULE_VTS_VERSION}.tar.gz" "https://github.com/vozlt/nginx-module-vts/archive/refs/tags/v${NGINX_MODULE_VTS_VERSION}.tar.gz"
+curl --fail -L -o "nginx-${NGINX_VERSION}.tar.gz" "https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
+curl --fail -L -o "nginx-module-vts-${NGINX_MODULE_VTS_VERSION}.tar.gz" "https://github.com/vozlt/nginx-module-vts/archive/refs/tags/v${NGINX_MODULE_VTS_VERSION}.tar.gz"
