85837554af70577fbc3a68a5c8b4cd9f69db72d4
ririnto <ririnto@kakao.com>
2026-02-27 22:25:47 +0900

    feat(download): 고정 체크섬 의존 제거
    로컬 체크섬 매니페스트 사용을 중단하고 HTTPS 체크섬 URL만 허용하도록 검증 경로를 정리한다.\nvector는 업스트림 SHA256SUMS를 사용하고, nginx/coredns/dnsmasq는 고정 체크섬 파일을 제거해 정책에 맞춘다.

--- dnsmasq/download.sh
+++ dnsmasq/download.sh
@@ -7,4 +7,4 @@ ROOT_DIR="$(CDPATH= cd -- "${MODULE_DIR}/.." && pwd)"
 . "${MODULE_DIR}/.env"
 . "${ROOT_DIR}/.github/scripts/common.sh"
 
-download_tarball "https://thekelleys.org.uk/dnsmasq/dnsmasq-${DNSMASQ_VERSION}.tar.gz" "${MODULE_DIR}/src/dnsmasq-${DNSMASQ_VERSION}.tar.gz" "${MODULE_DIR}/checksums/sha256sums.txt"
+download_tarball "https://thekelleys.org.uk/dnsmasq/dnsmasq-${DNSMASQ_VERSION}.tar.gz" "${MODULE_DIR}/src/dnsmasq-${DNSMASQ_VERSION}.tar.gz"
--- vector/download.sh
+++ vector/download.sh
@@ -7,4 +7,4 @@ ROOT_DIR="$(CDPATH= cd -- "${MODULE_DIR}/.." && pwd)"
 . "${MODULE_DIR}/.env"
 . "${ROOT_DIR}/.github/scripts/common.sh"
 
-download_tarball "https://github.com/vectordotdev/vector/releases/download/v${VECTOR_VERSION}/vector-${VECTOR_VERSION}-x86_64-unknown-linux-musl.tar.gz" "${MODULE_DIR}/src/vector-${VECTOR_VERSION}-x86_64-unknown-linux-musl.tar.gz" "${MODULE_DIR}/checksums/sha256sums.txt"
+download_tarball "https://github.com/vectordotdev/vector/releases/download/v${VECTOR_VERSION}/vector-${VECTOR_VERSION}-x86_64-unknown-linux-musl.tar.gz" "${MODULE_DIR}/src/vector-${VECTOR_VERSION}-x86_64-unknown-linux-musl.tar.gz" "https://github.com/vectordotdev/vector/releases/download/v${VECTOR_VERSION}/vector-${VECTOR_VERSION}-SHA256SUMS"
