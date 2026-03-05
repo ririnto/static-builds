e6dd93fbf2a63338580304c452c7961c2bb2b8ac
ririnto <ririnto@users.noreply.github.com>
2026-03-05 22:24:10 +0900

    chore: 타겟 다운로드 체크섬 인자/변수 제거

--- monit/download.sh
+++ monit/download.sh
@@ -7,4 +7,4 @@ ROOT_DIR="$(CDPATH= cd -- "${MODULE_DIR}/.." && pwd)"
 . "${MODULE_DIR}/.env"
 . "${ROOT_DIR}/.github/scripts/common.sh"
 
-download_tarball "https://mmonit.com/monit/dist/monit-${MONIT_VERSION}.tar.gz" "${MODULE_DIR}/src/monit-${MONIT_VERSION}.tar.gz" "https://mmonit.com/monit/dist/monit-${MONIT_VERSION}.tar.gz.sha256"
+download_tarball "https://mmonit.com/monit/dist/monit-${MONIT_VERSION}.tar.gz" "${MODULE_DIR}/src/monit-${MONIT_VERSION}.tar.gz"
